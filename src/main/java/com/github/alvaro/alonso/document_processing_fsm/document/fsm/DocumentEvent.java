bb07a90c190f5cd326110a44f31a946813b58391
Alvaro Alonso <35852638+AlvaroAlonsoFlor@users.noreply.github.com>
2025-06-13 17:40:06 +0100

    refactor: Use DocumentService to handle transitions and business logic (#1)

--- src/main/java/com/github/alvaro/alonso/document_processing_fsm/document/fsm/DocumentEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.alvaro.alonso.document_processing_fsm.document.fsm;
-
-import com.github.alvaro.alonso.document_processing_fsm.document.Document;
-import com.github.alvaro.alonso.document_processing_fsm.document.DocumentEventException;
-
-public interface DocumentEvent {
-    DocumentEventType getEventType();
-    void handle(Document document) throws DocumentEventException;
-}
---
+++ src/test/java/com/github/alvaro/alonso/document_processing_fsm/DocumentServiceTest.java
@@ -0,0 +1,26 @@
+package com.github.alvaro.alonso.document_processing_fsm;
+
+import com.github.alvaro.alonso.document_processing_fsm.document.Document;
+import com.github.alvaro.alonso.document_processing_fsm.document.DocumentService;
+import com.github.alvaro.alonso.document_processing_fsm.document.fsm.DocumentState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DocumentServiceTest {
+
+    private DocumentService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new DocumentService();
+    }
+
+    @Test
+    void shouldReturnSubmittedDocumentWithUpdatedStatus() {
+        Document document = new Document(null, "Title", "some content", "Some author", DocumentState.DRAFT, null);
+        Document updatedDocument = service.submitDraft(document);
+        assertEquals(updatedDocument, document.withState(DocumentState.SUBMITTED));
+    }
+}
