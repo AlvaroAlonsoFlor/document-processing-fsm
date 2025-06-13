d583aede4a62f357468030355adffe5c20bd935a
Alvaro <alvaroalonsoflor@gmail.com>
2025-06-13 23:16:58 +0100

    feat: add base validation rules to Document

--- src/main/java/com/github/alvaro/alonso/document_processing_fsm/document/Document.java
+++ src/main/java/com/github/alvaro/alonso/document_processing_fsm/document/Document.java
@@ -1,15 +1,32 @@
 package com.github.alvaro.alonso.document_processing_fsm.document;
 
 import com.github.alvaro.alonso.document_processing_fsm.document.fsm.DocumentState;
+import jakarta.validation.constraints.NotBlank;
 
 public record Document(
+
         String id,
+
+        @NotBlank
         String title,
+
+        @NotBlank
         String content,
+
+        @NotBlank
         String author,
+
+        @NotBlank
         DocumentState state,
+
+        @NotBlank(groups = ReviewValidation.class)
         String reviewer) {
     public Document withState(DocumentState state) {
         return new Document(id, title, content, author, state, reviewer);
     }
+
+    public interface ReviewValidation {}
 }
+
+
+
