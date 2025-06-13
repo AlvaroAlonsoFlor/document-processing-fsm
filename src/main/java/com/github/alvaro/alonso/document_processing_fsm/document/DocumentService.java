164b2a3eb96843c5b1897e351483bc0bbbcde396
Alvaro <alvaroalonsoflor@gmail.com>
2025-06-13 17:37:23 +0100

    refactor: Use DocumentService to handle transitions and business logic

--- src/main/java/com/github/alvaro/alonso/document_processing_fsm/document/DocumentService.java
+++ src/main/java/com/github/alvaro/alonso/document_processing_fsm/document/DocumentService.java
@@ -1,4 +1,26 @@
 package com.github.alvaro.alonso.document_processing_fsm.document;
 
+import com.github.alvaro.alonso.document_processing_fsm.document.fsm.DocumentEventType;
+import com.github.alvaro.alonso.document_processing_fsm.document.fsm.DocumentState;
+import com.github.alvaro.alonso.document_processing_fsm.document.fsm.DocumentStateMachine;
+
 public class DocumentService {
+    private final DocumentStateMachine stateMachine;
+
+
+    public DocumentService() {
+        stateMachine = new DocumentStateMachine();
+    }
+
+    public Document submitDraft(Document document) {
+        // assumes controller validates body fields
+        // find author exists
+        DocumentState currentState = document.state();
+        DocumentState newState = stateMachine.getNextState(currentState, DocumentEventType.SUBMIT);
+        // save new document and then return back object for controller
+        // maybe send notification
+        // use audit service to audit action
+        return document.withState(newState);
+
+    }
 }
